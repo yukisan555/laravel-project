ce5e1f54a1ea744c3501261a89d2fbd036aa5f49
yukisan555 <ykn931209@gmail.com>
2025-03-16 11:25:01 +0000

    db_books add

---
+++ database/migrations/2025_03_16_110810_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('bookname', 100);
+            $table->string('author', 60);
+            $table->string('publisher',100);
+            $table->date('publicationdate');
+            $table->text('content');
+            $table->string('image')->nullable()->after('imagename'); // 画像のファイル名（NULL許可）
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
